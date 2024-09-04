b405783d932dec0dfbd0dddc377befdf12c0ce1b
j03m4r <joeemarr12345@gmail.com>
2024-09-04 17:32:04 -0500

    Reconfiguring route protection.

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,9 +1,20 @@
 "use client";
 import SectionHeader from '@/components/typography/SectionHeader'
-import React from 'react'
+import React, { useEffect } from 'react'
 import SignInForm from '@/components/forms/SignInForm';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 export default function AdminPage() {
+    const session = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (session) {
+            router.push("/admin/films")
+        }
+    }, [session])
+
     return (
         <div className='mx-auto max-w-screen-lg h-screen flex flex-col'>
             <SectionHeader className='m-12'>
